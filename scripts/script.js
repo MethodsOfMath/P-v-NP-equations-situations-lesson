14940216cdaa7be820604d19a19278b74a8c3ec3
Scott Geyer <scott.fred.geyer@gmail.com>
2024-11-04 21:04:59 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 imageNumber = 1;
 height = 200;
 currentHeight = 300;
-key = [[1,true],[2,false],[3,false],[4,false],[5,false],[6,false],[7,true],[8,false],[9,false],[10,false],[11,false],[12,true],[13,false],[14,true],[15,false],[16,false],[17,false],[18,false],[19,true],[20,true],[21,true],[22,true],[23,false],[24,false],[25,true],[26,false],[27,false],[28,true],[29,false],[30,true],[31,false],[32,false],[33,false],[34,false],[35,false],[36,false],[37,true],[38,false],[39,true],[40,false],[41,false],[42,false]];
+key = [[1,true],[2,false],[3,false],[4,true],[5,true],[6,false],[7,false],[8,false],[9,true],[10,true],[11,false],[12,false],[13,true],[14,true],[15,false],[16,false],[17,true],[18,false],[19,true],[20,false]];
 
 yesImages = "<h1>Yes Images</h1>";
 noImages = "<h1>No Images</h1>";
@@ -10,7 +10,7 @@ noImages = "<h1>No Images</h1>";
 function displayImage(n) {
  let image ="<img src='https://raw.githubusercontent.com/MethodsOfMath/yes-no-game-proportional/refs/heads/master/images/" + n + ".png' height='"+ currentHeight + "'>";
  if (n === 1){
-   image += "<p>This is a yes. Click yes to begin.</p>";
+   image += "<p>This is proportional. Click yes to begin.</p>";
  }
   document.getElementById("current-image").innerHTML = image;
 }
@@ -22,7 +22,7 @@ function yes(){
   yesImages += "<img src='https://raw.githubusercontent.com/MethodsOfMath/yes-no-game-proportional/refs/heads/master/images/" + imageNumber + ".png' height='" + height + "'>";
   document.getElementById("yesImages").innerHTML = yesImages;
   } else {
-      let msg = "No, it's a no.";
+      let msg = "No, it's non-proportional.";
       document.getElementById("message").innerHTML = msg;
     noImages += "<img src='https://raw.githubusercontent.com/MethodsOfMath/yes-no-game-proportional/refs/heads/master/images/" + imageNumber + ".png' height='" + height + "'>";
   document.getElementById("noImages").innerHTML = noImages;
@@ -39,7 +39,7 @@ function yes(){
 
 function no(){ 
   if (key[imageNumber-1][1]) {
-    let msg = "No, it's a yes.";
+    let msg = "No, it's proportional.";
     document.getElementById("message").innerHTML = msg;
   yesImages += "<img src='https://raw.githubusercontent.com/MethodsOfMath/yes-no-game-proportional/refs/heads/master/images/" + imageNumber + ".png' height='" + height + "'>";
   document.getElementById("yesImages").innerHTML = yesImages;
